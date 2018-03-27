8d1272c50d666c940c08ae1bbde1d7a4aaf17a01
wangqc <wangqc@zetyun.com>
2018-03-27 12:23:21 +0800

    init backend service.

---
+++ backend/src/main/java/com/kst/CultureApplication.java
@@ -0,0 +1,42 @@
+package com.kst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableScheduling
+public class CultureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultureApplication.class, args);
+    }
+
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        return corsConfiguration;
+    }
+
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig()); // 4
+        return new CorsFilter(source);
+    }
+
+}
---
+++ backend/src/main/java/com/kst/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.kst.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @RequestMapping("")
+    public String index() {
+        String msg = "culture backend service is running...";
+        logger.info(msg);
+        return msg;
+    }
+
+}
