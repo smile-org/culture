9c0b45b053f6887322a16b99ac28898f77cef903
dhy <beijingdhy@126.com>
2018-05-12 14:23:37 +0800

    m-web

--- frontend/cms/src/views/homepage/hotTopic.vue
+++ frontend/cms/src/views/homepage/hotTopic.vue
@@ -61,7 +61,7 @@
                     :on-change="changeContentUpload"
                     :file-list="fileList">
                     <el-button size="small" type="primary">点击上传</el-button>
-                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb，图片尺寸 585*329</div>
+                    <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb，图片尺寸 585*366</div>
                   </el-upload>
                 </div>
               </li>
