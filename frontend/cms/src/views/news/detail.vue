56d56cd26081bc78c1a66bae24e21d32748f541e
dhy <beijingdhy@126.com>
2018-05-15 18:57:00 +0800

    新闻增加提示

--- frontend/cms/src/views/news/detail.vue
+++ frontend/cms/src/views/news/detail.vue
@@ -45,7 +45,7 @@
                     :on-change="changeContentUpload"
                     :file-list="fileList">
                   <el-button size="small" type="primary">点击上传</el-button>
-                  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+                  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb,图片尺寸 370*231</div>
                 </el-upload>
               </div>
               </li>
